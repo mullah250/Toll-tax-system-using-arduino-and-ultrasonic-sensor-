68f91894431b10d738072f94631eeb78ce49ae46
mullah250 <133054664+mullah250@users.noreply.github.com>
2023-05-11 02:25:56 +0200

    Add files via upload

---
+++ gilbert.ino
@@ -0,0 +1,38 @@
+#include<Servo.h>
+Servo myservo;
+const int trigPin=3;
+const int echoPin=5;
+long tmeduration;
+int distance;
+void setup() {
+  myservo.attach(9);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  Serial.begin(9600);
+
+
+
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  tmeduration=pulseIn(echoPin,HIGH);
+  distance=(0.034*tmeduration)/2;
+
+  if(distance<=50){
+    myservo.write(180);
+  }
+  else{
+    myservo.write(0);}
+
+    Serial.print("distance");
+    Serial.println(distance);
+
+
+    delay(2000);
+}
